22ca997afa9cee5df93c210a32d7a17efda44465
GalacticGlum <nerftactic09@gmail.com>
2020-01-18 12:38:35 -0500

    Added transcription tokenization using NLTK

--- backend/search_engine.py
+++ backend/search_engine.py
@@ -1,14 +1,24 @@
 import argparse
+from enum import Enum
 from pathlib import Path
 from datetime import timedelta
-import api.hash_util as hash_util
 
+import nltk
+import nltk.tokenize
+import api.hash_util as hash_util
 from google.cloud import speech_v1p1beta1 as speech, storage
 from google.cloud.speech_v1p1beta1 import enums
 from google.cloud.speech_v1p1beta1 import types
 
+class SearchMode(Enum):
+    WORD = 'word'
+    SENTENCE = 'sentence'
+    FRAGMENT = 'fragment'
+
 parser = argparse.ArgumentParser(description='The core search engine.')
 parser.add_argument('input', type=str, help='The path to the input audio file.')
+# parser.add_argument('search', type=str, help='The search term.')
+parser.add_argument('--search-mode', type=SearchMode, choices=list(SearchMode))
 parser.add_argument('--auth', dest='auth_json_filepath', type=str, help='The path to the service account credentials file.')
 args = parser.parse_args()
 
@@ -19,6 +29,9 @@ BUCKET_NAME = 'syft-audio-bucket'
 BUCKET_AUDIO_ROOT = ''
 # empty, directory, directory/
 bucket = storage_client.get_bucket(BUCKET_NAME)
+
+# Download the NLTK model (which should probably only do this once, on startup)
+nltk.download('punkt')
 
 with open(Path(args.input), 'rb') as audio_file:
     # Upload the file to GCS if it doesn't already exists
@@ -34,9 +47,10 @@ with open(Path(args.input), 'rb') as audio_file:
     blob_uri = 'gs://{}/{}'.format(BUCKET_NAME, blob_filename)
 
     config = types.RecognitionConfig(
-        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        encoding=enums.RecognitionConfig.AudioEncoding.ENCODING_UNSPECIFIED,
         sample_rate_hertz=16000,
         enable_speaker_diarization=True,
+        enable_automatic_punctuation=True,
         language_code='en-US')
 
     operation = speech_client.long_running_recognize(config, types.RecognitionAudio(uri=blob_uri))
@@ -44,6 +58,7 @@ with open(Path(args.input), 'rb') as audio_file:
     for result in op_result.results:
         print('=' * 20)
         print('{} alternative(s) found'.format(len(result.alternatives)))
-        print(result.alternatives[0].transcript)
-        print(result.alternatives[0].confidence)
-        print(result.alternatives[0].words)
+        # print(result.alternatives[0].transcript)
+        print(nltk.tokenize.sent_tokenize(result.alternatives[0].transcript))
+        # print(result.alternatives[0].confidence)
+        # print(result.alternatives[0].words)
